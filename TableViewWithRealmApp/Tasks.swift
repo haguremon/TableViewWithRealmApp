714d26664667ed96c8ed0ee8e192c7aeeed78482
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-16 17:37:58 +0900

    データベースとかが含まれるアプリとか複雑になるから好きRealmやりやすい良いい途中ですがセーブコミット7/16 17:37

---
+++ TableViewWithRealmApp/Tasks.swift
@@ -0,0 +1,13 @@
+//
+//  Tasks.swift
+//  TableViewWithRealmApp
+//
+//  Created by IwasakIYuta on 2021/07/16.
+//
+
+import Foundation
+import RealmSwift
+
+class Tasks : Object {
+    @objc dynamic var task = ""
+}
--- TableViewWithRealmApp/ViewController.swift
+++ TableViewWithRealmApp/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    
+    
+    
+    var tasks = [Tasks]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    
+    @IBAction func addTaskButton(_ sender: UIButton) {
+    
     }
 
-
+}
+extension ViewController: UITableViewDataSource , UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = tasks[indexPath.row].task
+     
+        return cell
+    }
 }
 
